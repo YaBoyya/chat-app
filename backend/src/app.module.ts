f87c10f51b7064e8e7fe64445bc181b76cd523ec
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-06-06 15:11:05 +0200

    Add postgreSQL config service and dotenv

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfigService } from './config/config.service';
 
 @Module({
-  imports: [ChatsModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfigService.getTypeOrmConfig()),
+    ChatsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
