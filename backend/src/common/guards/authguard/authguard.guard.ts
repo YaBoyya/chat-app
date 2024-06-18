fd098904652d4d256249df96195d35a2f31f175e
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-06-18 19:31:04 +0200

    Add AuthGuard and update packages

---
+++ backend/src/common/guards/authguard/authguard.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { AuthService } from "src/auth/auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+  constructor(private readonly authService: AuthService) {}
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const { authorization }: any = request.headers;
+      console.log(request.headers, authorization)
+      if (!authorization || authorization.trim() === ''){
+        throw new UnauthorizedException('Please provide authorization token.');
+      }
+      const authToken = authorization.replace(/bearer/gim, '').trim();
+      const resp = this.authService.validateToken(authToken);
+      console.log("resp", resp);
+      request.decodedData = resp;
+      return true;
+
+    } catch(error) {
+      console.log('auth error - ', error.message);
+      throw new ForbiddenException(error.message || 'Session expired! Please sign in.')
+    }
+  }
+}
