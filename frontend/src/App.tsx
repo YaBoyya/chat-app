71a418c2db5ce5e6d727790906399a4f55df459c
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-06-20 22:49:43 +0200

    Add very basic register form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Register() {
+  const initialState = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  }
+  const [registerData, setRegisterData] = useState(initialState);
 
+  const handleRegisterFormChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const {name, value} = e.target;
+    setRegisterData((prev) => ({...prev, [name]: value}));
+  }
+
+  function submitRegisterForm(e: any): void {
+    e.preventDefault();
+    e.stopPropagation()
+    setRegisterData(initialState);
+  }
+
+  // TODO Fix form submitting and update css
+  return (
+    <form>
+      <label>Username:</label>
+      <input
+        name="username"
+        type="text"
+        value={registerData.username}
+        onChange={handleRegisterFormChange}
+      ></input>
+      <label>Email:</label>
+      <input
+        name="email"
+        type="text"
+        value={registerData.email}
+        onChange={handleRegisterFormChange}
+        ></input>
+      <label>Password:</label>
+      <input
+        name="password"
+        type="password"
+        value={registerData.password}
+        onChange={handleRegisterFormChange}
+        ></input>
+      <label>Confirm password:</label>
+      <input
+        name="confirmPassword"
+        type="password"
+        value={registerData.confirmPassword}
+        onChange={handleRegisterFormChange}
+        ></input>
+      <button onClick={() => submitRegisterForm}>Register</button>
+    </form>
+  )
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Register />
     </>
   )
 }
