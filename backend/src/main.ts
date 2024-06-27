83546c71b93e06652de6403212e0ceea008a623c
Yaoyya <110597692+YaBoyya@users.noreply.github.com>
2024-06-27 11:12:35 +0200

    Revert nestjst clearing terminal on launch and add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(8000);
 }
 bootstrap();
